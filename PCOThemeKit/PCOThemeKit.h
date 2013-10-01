b6eac0dd61a530edcc0e6e898be9fa8780989c0b
Skylar Schipper <ss@schipp.co>
2013-10-01 16:18:21 -0700

    Initial Commit

---
+++ PCOThemeKit/PCOThemeKit.h
@@ -0,0 +1,13 @@
+//
+//  PCOThemeKit.h
+//  PCOThemeKit
+//
+//  Created by Skylar Schipper on 10/1/13.
+//  Copyright (c) 2013 Ministy Centered Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PCOThemeKit : NSObject
+
+@end
---
+++ PCOThemeKitTests/PCOThemeKitTests.m
@@ -0,0 +1,34 @@
+//
+//  PCOThemeKitTests.m
+//  PCOThemeKitTests
+//
+//  Created by Skylar Schipper on 10/1/13.
+//  Copyright (c) 2013 Ministy Centered Technology. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PCOThemeKitTests : XCTestCase
+
+@end
+
+@implementation PCOThemeKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
